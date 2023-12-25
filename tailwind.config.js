1336c75741425ceaa9c5f848ace393e9540e9ad2
Jagmit Gabba <jagmitg@gmail.com>
2023-12-25 14:29:47 +0000

    Added to Local storage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
     content: ["./src/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {}
     },
-    plugins: []
+    plugins: [require("@tailwindcss/forms")]
 };
