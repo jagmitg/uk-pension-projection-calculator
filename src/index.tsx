a775b3f11bed4856ef7568928ee8fcfab7f0b2e1
Jagmit Gabba <jagmitg@gmail.com>
2023-12-25 12:51:12 +0000

    Changes to the typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 import { createRoot } from 'react-dom/client';
 
-import { App } from './App';
+const rootElement = document.getElementById('app');
+if (!rootElement) throw new Error('Failed to find the root element');
 
-const root = createRoot(document.getElementById('app'));
+const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App name="StackBlitz" />
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
